510672dfecd18cc422debbbc84d6196f9c6e0d60
weam ibrahim <weamibrahim46@gmail.com>
2024-04-06 22:18:17 +0200

    update for deploy

--- frontend/src/app/graphql.module.ts
+++ frontend/src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import { HttpLink } from 'apollo-angular/http';
 import { NgModule } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 
-const url = 'http://localhost:3000/api/graphql';
+const url = 'https://posts-mr5e.onrender.com/api/graphql';
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
